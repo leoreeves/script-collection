1be4b4da593c8733eb05d0c10125382552f00c0f
Leo Reeves <leojreeves@gmail.com>
2016-09-25 18:37:06 +0100

    Bug fix
    Fixed error

--- character_finder.py
+++ character_finder.py
@@ -1,11 +1,10 @@
 # finds a character in a word
 
 word = input("Word: ")
-print("Please enter required character below:")
 
 while True:
-    count = int(input("character: ")) - 1
+    count = int(input("Character: ")) - 1
     if count > len(word):
         print("Invalid character")
     else:
-        print ((str(int(count) + 1)+ " = " + password[count]))
+        print ((str(int(count) + 1)+ " = " + word[count]))
